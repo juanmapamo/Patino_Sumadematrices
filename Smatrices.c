741de433db78288c4a07d47a7b8b7534fa5e310a
Juan Manuel Patiño <juan.patino@udla.edu.ec>
2023-11-29 15:52:54 -0500

    Tercera linea de codigo

--- Smatrices.c
+++ Smatrices.c
@@ -20,8 +20,27 @@ int main (void)
         matriz [i][j]=rand()%101;
        }
     }
-   
-   
+    printf("Primera matriz\n");
+
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            printf("%d\t", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+    int Segundamatriz[a][b];
+ 
+    for (int i = 0; i < a; i++)
+    {
+       for (int j = 0; j < b; j++)
+       {
+        Segundamatriz[i][j]=rand()%101;
+       }
+    }
+
     
     return 0;
 }
