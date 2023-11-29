50182faf83e3cd4ee7a96de2da35400eba077db8
Juan Manuel Patiño <juan.patino@udla.edu.ec>
2023-11-29 15:56:26 -0500

    Ultima linea de codigo

--- Smatrices.c
+++ Smatrices.c
@@ -7,12 +7,15 @@ int main (void)
     
     srand (time (NULL));
 
+    //En esta seccion decimos al ususario que ingrese cuantas filas y cuantas columnas va a desear.
     int a,b;
     printf("Ingrese el tamanio de la fila\n");
     scanf("%d", &a);
     printf("Ingrese el tamanio de la columna\n");
     scanf("%d", &b);
     int matriz[a][b];
+
+    //En esta seccion llamamos a la Primera matriz para que nos de numeros aleatorios del 0 al 100.
     for (int i = 0; i < a; i++)
     {
        for (int j = 0; j < b; j++)
@@ -32,7 +35,9 @@ int main (void)
     }
 
     int Segundamatriz[a][b];
- 
+
+    //En esta seccion llamamos a la Segunda matriz para que nos de numeros aleatorios del 0 al 100.
+
     for (int i = 0; i < a; i++)
     {
        for (int j = 0; j < b; j++)
@@ -41,6 +46,40 @@ int main (void)
        }
     }
 
-    
+    printf("Segunda matriz\n");
+
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            printf("%d\t", Segundamatriz[i][j]);
+        }
+        printf("\n");
+    }
+
+    int Sumamatriz[a][b];
+
+    //En esta seccion hacemos la suma de la primera y la segunda matriz.
+
+    for (int i = 0; i < a; i++)
+    {
+       for (int j = 0; j < b; j++)
+       {
+        Sumamatriz[i][j]=matriz[i][j] + Segundamatriz[i][j];
+       }
+    }
+
+    //En esta seccion imprmimos el resultado de las dos matrices.
+
+    printf("Resultado de la suma de matrices\n");
+
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            printf("%d\t", Sumamatriz[i][j]);
+        }
+        printf("\n");
+    }
     return 0;
 }
